21dc1ad3f0e804842aceec3bda4224baa4d1a902
Viktor Wilhelm <wilhelm.viktor78@gmail.com>
2026-08-31 21:55:02 +0200

    Revert "fix: update event listener for card flip functionality in main.ts"
    This reverts commit d68ff23b411131fe2972bec6292f991a07417a1e.

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,4 @@ import './styles/style.scss';
 init()
 
 function init() {
-    const fieldRef = document.getElementById('field')
-    if (fieldRef) {
-        fieldRef.addEventListener('click', e  => {
-            const card = (e.target as HTMLElement).closest('.card') as HTMLButtonElement
-            if(card) {
-                card.classList.toggle('is-flipped')
-            }
-        })
-    }
-}
-    
-            
+    const fieldRef = document.getElementById('field') as HTMLInputElement;
