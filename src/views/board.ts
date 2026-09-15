d7d68ca35e9dbe701f9c987f5295ec5dc2ff5716
Viktor Wilhelm <wilhelm.viktor78@gmail.com>
2026-09-15 15:08:29 +0200

    fix: match Code vibes exit button hover to Figma

--- src/views/board.ts
+++ src/views/board.ts
@@ -26,6 +26,14 @@ const EXIT_ICON = `
   </svg>
 `;
 
+// Code vibes-only exit icon, matching its Figma reference (docs/figma-reference
+// board icon export); other themes keep EXIT_ICON above.
+const CODE_VIBES_EXIT_ICON = `
+  <svg width="26" height="23" viewBox="0 0 26 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M21.4375 12.5H7.5C7.14583 12.5 6.84896 12.3802 6.60938 12.1406C6.36979 11.901 6.25 11.6042 6.25 11.25C6.25 10.8958 6.36979 10.599 6.60938 10.3594C6.84896 10.1198 7.14583 10 7.5 10H21.4375L20.375 8.9375C20.125 8.6875 20.0052 8.39583 20.0156 8.0625C20.026 7.72917 20.1458 7.4375 20.375 7.1875C20.625 6.9375 20.9219 6.80729 21.2656 6.79688C21.6094 6.78646 21.9062 6.90625 22.1562 7.15625L25.375 10.375C25.625 10.625 25.75 10.9167 25.75 11.25C25.75 11.5833 25.625 11.875 25.375 12.125L22.1562 15.3438C21.9062 15.5938 21.6094 15.7135 21.2656 15.7031C20.9219 15.6927 20.625 15.5625 20.375 15.3125C20.1458 15.0625 20.026 14.7708 20.0156 14.4375C20.0052 14.1042 20.125 13.8125 20.375 13.5625L21.4375 12.5ZM15 6.25V2.5H2.5V20H15V16.25C15 15.8958 15.1198 15.599 15.3594 15.3594C15.599 15.1198 15.8958 15 16.25 15C16.6042 15 16.901 15.1198 17.1406 15.3594C17.3802 15.599 17.5 15.8958 17.5 16.25V20C17.5 20.6875 17.2552 21.276 16.7656 21.7656C16.276 22.2552 15.6875 22.5 15 22.5H2.5C1.8125 22.5 1.22396 22.2552 0.734375 21.7656C0.244792 21.276 0 20.6875 0 20V2.5C0 1.8125 0.244792 1.22396 0.734375 0.734375C1.22396 0.244792 1.8125 0 2.5 0H15C15.6875 0 16.276 0.244792 16.7656 0.734375C17.2552 1.22396 17.5 1.8125 17.5 2.5V6.25C17.5 6.60417 17.3802 6.90104 17.1406 7.14062C16.901 7.38021 16.6042 7.5 16.25 7.5C15.8958 7.5 15.599 7.38021 15.3594 7.14062C15.1198 6.90104 15 6.60417 15 6.25Z" fill="currentColor" />
+  </svg>
+`;
+
 export function renderBoard(): HTMLElement {
   const state = getState();
   const boardSize = BOARD_SIZES[state.boardSize];
@@ -37,6 +45,7 @@ export function renderBoard(): HTMLElement {
     .join('');
 
   const scoreIcon = theme.boardScoreIcon === 'flag' ? FLAG_ICON : PAWN_ICON;
+  const exitIcon = state.theme === 'code-vibes' ? CODE_VIBES_EXIT_ICON : EXIT_ICON;
   const scoreItems = theme.boardScoreOrder
     .map((color) => {
       const label = theme.boardScoreShowLabel ? `${PLAYERS[color].label} ` : '';
@@ -70,7 +79,7 @@ export function renderBoard(): HTMLElement {
         Current player:
         <span class="board__current-player-badge board__current-player-badge--${state.playerColor}${currentPlayerModifier}">${scoreIcon}</span>
       </div>
-      <button type="button" class="board__exit">${EXIT_ICON}Exit game</button>
+      <button type="button" class="board__exit">${exitIcon}Exit game</button>
     </div>
 
     <div class="board__grid-wrap">
